0ef1d09423a6362c264c525f075f8fc6b0c336d5
Saeed Hossen <appsaeed7@gmail.com>
2024-05-08 01:19:32 +0600

    add scan all dir

--- public/scan.php
+++ public/scan.php
@@ -1,8 +1,10 @@
 <?php
 try {
-    $path = '/var/www/html';
+    $path = isset( $_REQUEST['path'] ) ? $_REQUEST['path'] : '/var/www/html';
     $files = scandir( $path );
+    echo '<pre>';
     print_r( $files );
+    echo '</pre>';
 } catch ( \Throwable $th ) {
     print_r( $th->getMessage() );
 }
