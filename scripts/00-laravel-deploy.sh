11ff29f136e45950795a2399885ea574f6c1d1db
Saeed Hossen <appsaeed7@gmail.com>
2024-05-08 04:51:08 +0600

    update: scripts and env

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,7 +2,7 @@
 echo "Running composer"
 # composer install --no-dev --working-dir=/var/www/html
 composer install --working-dir=/var/www/html
-mv /var/www/html/.env.deploy /var/www/html/.env
+cp /var/www/html/.env.deploy /var/www/html/.env
 
 echo "generating application key..."
 php artisan key:generate
