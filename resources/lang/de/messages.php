2ece4c548043fd55a3688b7e31da6eb98d2aeec3
Saeed Hossen <appsaeed7@gmail.com>
2024-05-09 00:36:09 +0600

    add bangla lang

--- resources/lang/de/messages.php
+++
@@ -1,9 +0,0 @@
-<?php 
-return [
-  'name' => 'Name',
-  'Add new todo' => 'Neue Aufgabe hinzufügen',
-  'Assign to' => 'Zuweisen',
-  'deadline' => 'Frist',
-  'set_deadline' => 'Frist festlegen',
-  'created_by' => 'Erstellt von',
-];
