78b255d4c7643ff2d396401f85fc392f50dd1cf0
Saeed Hossen <appsaeed7@gmail.com>
2024-05-11 00:30:36 +0600

    docker update permission for bootstrap/ storage

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer install --working-dir=/var/www/html
 
+echo "update permissions to storage and bootstrap";
+sudo chown -R www-data:www-data /var/www/html/bootstrap /var/www/html/storage
+
 echo "generating application key..."
 php artisan key:generate
 
